428611d62fc1a4bb14a58d55606aa11378e4325c
petra-scheuer <petra.scheuer.p@gmail.com>
2025-01-11 07:53:45 +0100

    Habe das test Projekt endlich hinzufügen können

---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true); // Dies ist ein erfolgreicher Testfall
+    }
+
+    [Theory]
+    [InlineData(2, 3, 5)] // Beispiel einer parametrisierten Eingabe
+    [InlineData(1, 1, 2)]
+    public void AddNumbers_TheoryTest(int a, int b, int expected)
+    {
+        int result = a + b;
+        Assert.Equal(expected, result); // Überprüfe, ob die Summe korrekt ist
+    }
+}
